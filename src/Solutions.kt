ea7cb9e54c599c205ef18391615334d53f771d4e
Iaremko Roman <salvoronis@gmail.com>
2021-10-11 02:03:14 +0300

    kotlin num 27

--- src/Solutions.kt
+++ src/Solutions.kt
@@ -1,5 +1,8 @@
+import kotlin.math.abs
+
 fun main() {
     println("Максимальный палиндром, полученный перемножением 2-х 3-х значных чисел, полученный в котлине -> ${fourth()}")
+    println("(A, B, Max prime number) -> ${primeSolution()}")
 }
 
 fun fourth() :Int {
@@ -23,4 +26,44 @@ val reverseNumber: (Int) -> Int = { x ->
         a /= 10
     }
     reversed
+}
+
+fun primeSolution() :Triple<Int, Int, Int> {
+    var bestNum = 0
+    var bestA = 0
+    var bestB = 0
+    for (a in -1000..1000) {
+        for (b in -1000..1000) {
+            val num = numberOfPrimes(a, b)
+            if (num > bestNum) {
+                bestNum = num
+                bestA = a
+                bestB = b
+            }
+        }
+    }
+    return Triple(bestA, bestB, bestNum)
+}
+
+fun numberOfPrimes(a :Int, b:Int) :Int {
+    val res: (Int, Int, Int) -> Int = {a, b, n -> n*n + n*a + b}
+    for (i in 0..80) {
+        if (!isPrime(res(a, b, i))) {
+            return i
+        }
+    }
+    return 0
+}
+
+fun isPrime(n :Int) :Boolean {
+    val num = abs(n)
+    if (num == 1 || num == 0) return false
+    var i = 2
+    while (i < num) {
+        if (num % i == 0) {
+            return false
+        }
+        i++
+    }
+    return true
 }
