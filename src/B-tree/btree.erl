3462fe41cc3c6d80cd6028013d724b40297cfd8b
Iaremko Roman <salvoronis@gmail.com>
2021-11-22 22:06:59 +0300

    monoid functions & filter

--- src/B-tree/btree.erl
+++ src/B-tree/btree.erl
@@ -12,7 +12,7 @@
 %% API
 -export([start/0]).
 
--record(node, {left, right = undefined, height, val}).
+-record(node, {left, right = undefined, height = 1, val}).
 
 init_btree(Val) -> #node{left = undefined, right = undefined, val = Val, height = 1}.
 
@@ -20,22 +20,21 @@ add(Node, Val) when is_record(Node, node) ->
   ResNode = case Node#node.val >= Val of
     true ->
       case Node#node.left of
-        Left when is_record(Left, node) -> left = Node#node{left = add(Left, Val)};
-        undefined -> Node#node{left = #node{left = undefined, right = undefined, val = Val, height = Node#node.height + 1}}
+        Left when is_record(Left, node) -> Node#node{left = add(Left, Val)};
+        undefined -> Node#node{left = #node{left = undefined, right = undefined, val = Val}}%%Node#node.height + 1}}
       end;
     false ->
       case Node#node.right of
         Right when is_record(Right, node) -> Node#node{right = add(Right, Val)};
-        undefined -> Node#node{right = #node{left = undefined, right = undefined, val = Val, height = Node#node.height + 1}}
+        undefined -> Node#node{right = #node{left = undefined, right = undefined, val = Val}}%%Node#node.height + 1}}
       end
   end,
-  balance(ResNode).
+  balance(ResNode);
+add(_, Val) -> init_btree(Val).
 
-height(Node) ->
-  case Node of
-    undefined -> 0;
-    Result -> Result#node.height
-  end.
+height(undefined) -> 0;
+height(Node) when is_record(Node, node)->
+  Node#node.height.
 
 balance_factor(Node) when is_record(Node, node) ->
   height(Node#node.right) - height(Node#node.left).
@@ -62,6 +61,7 @@ rotate_left(Node) when is_record(Node, node) ->
   PSuperNew = PNew#node{height = fix_height(PNew)},
   PSuperNew.
 
+balance(undefined) -> undefined;
 balance(Node) when is_record(Node, node) ->
   NodeNew = Node#node{height = fix_height(Node)},
   case balance_factor(NodeNew) of
@@ -90,7 +90,7 @@ print_btree(Prefix, Node, IsLeft) when is_record(Node, node) ->
     false -> io:format("r───");
     true -> io:format("l───")
   end,
-  io:format("~w|~w~n", [Node#node.val, Node#node.height]),
+  io:format("~w~n", [Node#node.val]),
   print_btree(Prefix ++ case IsLeft of true -> "│    "; false -> "     " end, Node#node.left, true),
   print_btree(Prefix ++ case IsLeft of true -> "│    "; false -> "     " end, Node#node.right, false);
 print_btree(_, _, _) -> ok.
@@ -106,9 +106,10 @@ remove_min(Node) when is_record(Node, node), Node#node.left == undefined ->
 remove_min(Node) when is_record(Node, node) ->
   balance(Node#node{left = remove_min(Node#node.left)}).
 
+remove(Node, Key) when Node == undefined ->
+  Node;
 remove(Node, Key) ->
   New = case Node of
-    undefined -> ok;
     Node when Key < Node#node.val -> Node#node{left = remove(Node#node.left, Key)};
     Node when Key > Node#node.val -> Node#node{right = remove(Node#node.right, Key)};
     Node when Node#node.val == Key ->
@@ -124,15 +125,74 @@ remove(Node, Key) ->
   end,
   balance(New).
 
+get(Node, Key) ->
+  get(Node, Key, undefined).
+get(Node, Key, Parent) ->
+  case Node of
+    undefined -> false;
+    Node when Key < Node#node.val -> get(Node#node.left, Key, Node);
+    Node when Key > Node#node.val -> get(Node#node.right, Key, Node);
+    Node when Key == Node#node.val -> {Node, Parent}
+  end.
+
+
+
+foldl(Fun, Acc, Node) ->
+  case Node of
+    undefined -> Acc;
+    _ ->
+      Acc0 = foldl(Fun, Acc, Node#node.left),
+      NewAcc = Fun(Node#node.val, Acc0),
+      foldl(Fun, NewAcc, Node#node.right)
+  end.
+
+foldr(Fun, Acc, Node) ->
+  case Node of
+    undefined -> Acc;
+    _ ->
+      Acc0 = foldr(Fun, Acc, Node#node.right),
+      NewAcc = Fun(Node#node.val, Acc0),
+      foldr(Fun, NewAcc, Node#node.left)
+  end.
+
+map(Fun, Node) ->
+  map(Fun, Node, ok).
+map(_, Node, NewNode) when Node == undefined ->NewNode;
+map(Fun, Node, NewNode)->
+  SuperNewNode = map(Fun, Node#node.left, NewNode),
+  NewNewNode = add(SuperNewNode, Fun(Node#node.val)),
+  map(Fun, Node#node.right, NewNewNode).
+
+filter(Fun, Node) -> filter(Fun, Node, ok).
+filter(_, Node, NewNode) when Node == undefined -> NewNode;
+filter(Fun, Node, NewNode) ->
+  SuperNewNode = filter(Fun, Node#node.left, NewNode),
+  NewNewNode = case Fun(Node#node.val) of
+    true -> add(SuperNewNode, Node#node.val);
+    false -> SuperNewNode
+  end,
+  filter(Fun, Node#node.right, NewNewNode).
+
+addTree(undefined, undefined) -> undefined;
+addTree(undefined, Tree) -> Tree;
+addTree(Tree, undefined) -> Tree;
+addTree(MainTree, SlaveTree) ->
+  SuperNewMainTree = addTree(MainTree, SlaveTree#node.left),
+  SuperPuperNewMainTree = add(SuperNewMainTree, SlaveTree#node.val),
+  addTree(SuperPuperNewMainTree, SlaveTree#node.right).
+
+subTree(undefined, undefined) -> undefined;
+subTree(undefined, Tree) -> Tree;
+subTree(Tree, undefined) -> Tree;
+subTree(MainTree, SlaveTree) ->
+  SuperNewMainTree = subTree(MainTree, SlaveTree#node.left),
+  SuperPuperNewMainTree = remove(SuperNewMainTree, SlaveTree#node.val),
+  subTree(SuperPuperNewMainTree, SlaveTree#node.right).
+
 start() ->
-  Tree0 = init_btree(2),
-  io:format("~w~n",[Tree0]),
-  Tree1 = add(Tree0, 1),
-  io:format("~w~n",[Tree1]),
+  Tree0 = init_btree(6),
+  Tree1 = add(Tree0, 2),
   Tree2 = add(Tree1, 3),
-  io:format("~w~n",[Tree2]),
   Tree3 = add(Tree2, 4),
-  io:format("~w~n",[Tree3]),
-  print_btree(Tree3),
-  TreeRm = remove(Tree3, 4),
-  print_btree(TreeRm).
+  Tree4 = add(Tree3, 5),
+  print_btree(filter(fun(X) -> case X of 3 -> false; _ -> true end end, Tree4)).
